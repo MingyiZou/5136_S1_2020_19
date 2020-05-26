c953619008ce234e06450fae8749e7b2251ce73a
HengWang-FIT5136 <63326536+HengWang-FIT5136@users.noreply.github.com>
2020-05-26 14:19:59 +1000

    Add files via upload

--- EmploymentDeployment.java
+++ EmploymentDeployment.java
@@ -1,5 +1,32 @@
 package missonToMars;
 
 public class EmploymentDeployment {
-
+	private String title;
+	private int number_required;
+	
+	public EmploymentDeployment()
+	{
+		title = "Unknown";
+		number_required = 0;
+	}
+	
+	public String getEmpTitle()
+	{
+		return title;
+	}
+	
+	public int getEmpReq()
+	{
+		return number_required;
+	}
+	
+	public void setEmpTitle(String newTitle)
+	{
+		title = newTitle;
+	}
+	
+	public void setNumReq(int newNumReq)
+	{
+		number_required = newNumReq;
+	}
 }
