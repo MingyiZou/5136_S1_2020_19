d48ce2bbec9b3e28e45eab4d206b9c5d081ae1a8
MingyiZou <65589625+yeeqwq@users.noreply.github.com>
2020-06-06 03:33:03 +1000

    Add files via upload

--- findCandidate.java
+++ findCandidate.java
@@ -1,12 +1,9 @@
-package missonToMars;
+package MissionToMars;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.*;
 import java.util.*;
-
-public class findCandidate{
-
-
+public class findCandidate {
     private static String filename;
     private static ArrayList<String> candidate;
     private static ArrayList<String> qualified_candidate;
@@ -14,16 +11,15 @@ public class findCandidate{
     private static int max_age;
     private static int min_age;
     private static int years_of_work_experience;
-    private static Mission mission;
+    private static mission mission;
 
 
-    public ArrayList<String> selectionCandidate(SelectionCriteria criteria){
+    public static ArrayList<String> selectionCandidate(SelectionCriteria criteria){
         //get selection criteria
         boolean health_record = criteria.getHealth_records();
         int max_age = criteria.getMaxAge();
         int min_age = criteria.getMinAge();
         int years_of_work_experience = criteria.getYears_of_work_experience();
-
 
         // get candidate list
         candidate = readFile("candidate.txt");
@@ -33,33 +29,33 @@ public class findCandidate{
         int age;
         boolean healthRecord;
         int work_years;
-
-        ArrayList<String> qualified_candidate = new ArrayList<>();
+        ArrayList<String> qualified_candidate = new ArrayList<String>();
         for (int i = 0; i < candidate.size(); i++){
-        	String[] candidateInformation = candidate.get(i).split(",");
-        	id = Integer.parseInt(candidateInformation[0]);
-        	name = candidateInformation[1];
-        	age = Integer.parseInt(candidateInformation[2]);
-        	healthRecord = Boolean.parseBoolean(candidateInformation[3]);
-        	work_years = Integer.parseInt(candidateInformation[4]);
+            String[] candidateInformation = candidate.get(i).split(",");
+            id = Integer.parseInt(candidateInformation[0]);
+            name = candidateInformation[1];
+            age = Integer.parseInt(candidateInformation[2]);
+            healthRecord = Boolean.parseBoolean(candidateInformation[3]);
+            work_years = Integer.parseInt(candidateInformation[4]);
 
-
-
-        	if (health_record == healthRecord && min_age < age && age > max_age && work_years > years_of_work_experience){
-                    qualified_candidate.add(name);
+            if (health_record == healthRecord && min_age < age && age < max_age && work_years > years_of_work_experience){
+                qualified_candidate.add(name);
             }
         }
-        System.out.println(qualified_candidate);
+        for(int k=0;k<qualified_candidate.size();k++)
+        {
+        System.out.println(qualified_candidate.get(k));
+        }
 
         //check if the candidate list is enough for the number required.
-        if (qualified_candidate.size() < mission.getEmployment_requirements().getEmpReq()){
-            System.out.println("Qualified Candidate in not enough. Please modify the selection Criteria");
-        }
+//        if (qualified_candidate.size() < mission.getEmployment_requirements().getEmpReq()){
+  //          System.out.println("Qualified Candidate in not enough. Please modifythe selection Criteria");
+    //    }
         return qualified_candidate;
     }
 
 
-    public ArrayList<String> readFile(String fileName)
+    public static ArrayList<String> readFile(String fileName)
     {
         ArrayList<String> candidates = new ArrayList<>();
         try
@@ -72,7 +68,7 @@ public class findCandidate{
                 {
                     candidates.add(parser.nextLine());
                 }
-                System.out.println(fileName + " is read successfully! ");
+           //     System.out.println(fileName + " is read successfully! ");
             }
             finally
             {
@@ -89,7 +85,4 @@ public class findCandidate{
         }
         return candidates;
     }
-
-
-
 }
